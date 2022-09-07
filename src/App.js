a858fcc714f9247c78dba670f3a585b4aa7001a6
tiyeii <dariotj@gmail.com>
2022-09-07 10:55:26 -0500

    Boton elminar
    se agrega funcionalidad al boton eliminar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { isEmpty} from "lodash"
+import { isEmpty, size} from "lodash"
 import shortid from "shortid";
 
 function App() {
@@ -23,6 +23,11 @@ function App() {
     setTask("")
     
   }
+
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
+  }
   return (
     <div className="container mt-5">
       <h1>Tareas</h1>
@@ -30,17 +35,34 @@ function App() {
       <div className="row">
         <div className="col-8">
           <h4 className="text-center">Lista de Tareas</h4>
+          { 
+          size(tasks) == 0 ? (
+            <h5 className="text-center">Aun no hay tareas programadas.</h5>
+
+             ): (
+
+             
+          
           <ul className="list-group"> 
               {
                 tasks.map((task)=> (
                 <li className="list-group-item" key={task.id}>
                 <span className="lead">{task.name}</span>
-                <button className="btn btn-danger btn-sm float-right mx-2"> Eliminar</button>
-                <button className="btn btn-warning btn-sm float-right">Editar</button>
+                <button 
+                className="btn btn-danger btn-sm float-right mx-2"
+                onClick={() => deleteTask(task.id)}
+                > 
+                Eliminar</button>
+                <button 
+                className="btn btn-warning btn-sm float-right">
+                  Editar</button>
               </li>
               ))
               }   
           </ul>
+          )
+          }
+
         </div>
 
         <div className="col-4">
